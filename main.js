be269968be6c2275e819dd7c121afc8a49a6e9fb
Rehan S. Grewal <88488978+R-S-GREWAL@users.noreply.github.com>
2023-07-20 20:50:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,94 @@
+
+function Login()
+{
+    window.location = "index.html";
+}
+function causes()
+{
+    window.location = "causes.html";
+}
+function test()
+{
+    window.location = "test.html";
+}
+function intro1() {
+  var dots1 = document.getElementById("dots1");
+  var moreText1 = document.getElementById("sym1");
+  var btnText1 = document.getElementById("btnintro1");
+
+  if (dots1.style.display === "none") {
+    dots1.style.display = "inline";
+    btnText1.innerHTML = "Read more";
+    moreText1.style.display = "none";
+  } else {
+    dots1.style.display = "none";
+    btnText1.innerHTML = "Read less";
+    moreText1.style.display = "inline";
+  }
+}
+function intro2() {
+  var dots2 = document.getElementById("dots2");
+  var moreText2 = document.getElementById("sym2");
+  var btnText2 = document.getElementById("btnintro2");
+
+  if (dots2.style.display === "none") {
+    dots2.style.display = "inline";
+    btnText2.innerHTML = "Read more";
+    moreText2.style.display = "none";
+  } else {
+    dots2.style.display = "none";
+    btnText2.innerHTML = "Read less";
+    moreText2.style.display = "inline";
+  }
+}
+function intro3() {
+  var dots3 = document.getElementById("dots3");
+  var moreText3 = document.getElementById("sym3");
+  var btnText3 = document.getElementById("btnintro3");
+
+  if (dots3.style.display === "none") {
+    dots3.style.display = "inline";
+    btnText3.innerHTML = "Read more";
+    moreText3.style.display = "none";
+  } else {
+    dots3.style.display = "none";
+    btnText3.innerHTML = "Read less";
+    moreText3.style.display = "inline";
+  }
+}
+function intro4() {
+  var dots4 = document.getElementById("dots4");
+  var moreText4 = document.getElementById("sym4");
+  var btnText4 = document.getElementById("btnintro4");
+
+  if (dots4.style.display === "none") {
+    dots4.style.display = "inline";
+    btnText4.innerHTML = "Read more";
+    moreText4.style.display = "none";
+  } else {
+    dots4.style.display = "none";
+    btnText4.innerHTML = "Read less";
+    moreText4.style.display = "inline";
+  }
+}
+function intro5() {
+  var dots5 = document.getElementById("dots5");
+  var moreText5 = document.getElementById("sym5");
+  var btnText5 = document.getElementById("btnintro5");
+
+  if (dots5.style.display === "none") {
+    dots5.style.display = "inline";
+    btnText5.innerHTML = "Read more";
+    moreText5.style.display = "none";
+  } else {
+    dots5.style.display = "none";
+    btnText5.innerHTML = "Read less";
+    moreText5.style.display = "inline";
+  }
+}
+
+
+  
+
+
+  
